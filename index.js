f882faf52cc92bea618c73e27a98b589d7d55330
Ionut Alexandru Tudor-Pricop <81566145+BLITzStudio@users.noreply.github.com>
2021-11-08 17:09:44 +0200

    comments.ejs page is ready

--- index.js
+++ index.js
@@ -4,16 +4,30 @@ const method_override = require("method-override");
 
 const app = express();
 const port = 8080;
+const comments = [
+  {
+    user:"user",
+    text:"I am a user"
+  }
+]
 
 app.set("view engine ","ejs");
 app.use(express.static(path.join(__dirname,"./node-modules/bootstrap/dist")));
-app.use(express.static(path.join(__dirname,"public")));
+app.use(express.static(path.join(__dirname,"/public")));
+app.use(method_override("_method"))
 
 
 
 app.get("/",(req,res)=>{
   res.render("index")
 })
+app.get('/comments',(req,res)=>{
+  res.render("comments",{comments})
+})
+
+
+
+
 app.listen(port,()=>{
   console.log(`App is listening on port ${port}`);
 })
