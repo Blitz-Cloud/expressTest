78f975e696ed303e8f5b56b52695f237975dcf63
Ionut Alexandru Tudor-Pricop <81566145+BLITzStudio@users.noreply.github.com>
2021-11-08 15:45:46 +0200

    This is stable state of the project

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require("express");
+const path = require("path");
+const method_override = require("method-override");
 
 const app = express();
 const port = 8080;
 
 app.set("view engine ","ejs");
-app.use(express.static("./node-modules/bootstrap/dist"))
+app.use(express.static(path.join(__dirname,"./node-modules/bootstrap/dist")));
+app.use(express.static(path.join(__dirname,"public")));
+
 
 
 app.get("/",(req,res)=>{
